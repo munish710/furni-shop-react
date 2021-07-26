7b3bb09c0472efbfebc62097311ecd4ab621740b
munish710 <munish710@gmail.com>
2021-07-27 00:38:10 +0530

    added utility function to format price

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,9 @@
-export const formatPrice = () => {};
+export const formatPrice = (number) => {
+  const newNumber = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "INR",
+  }).format((number * 50) / 100);
+  return newNumber;
+};
 
 export const getUniqueValues = () => {};
