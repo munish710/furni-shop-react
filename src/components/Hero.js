bbad229a9d93ee161f8f268988b13745ef249510
munish710 <munish710@gmail.com>
2021-07-26 16:37:12 +0530

    added Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,27 @@ import heroBcg from "../assets/hero-bcg.jpeg";
 import heroBcg2 from "../assets/hero-bcg-2.jpeg";
 
 const Hero = () => {
-  return <h4>hero</h4>;
+  return (
+    <Wrapper className="section-center">
+      <article className="content">
+        <h1>
+          Design your <br />
+          Comfort Zone
+        </h1>
+        <p>
+          Find the perfect piece of furniture for your day to day needs, making
+          elegance functional
+        </p>
+        <Link to="/products" className="btn hero-btn">
+          Shop Now
+        </Link>
+      </article>
+      <article className="img-container">
+        <img src={heroBcg} alt="table" className="main-img" />
+        <img src={heroBcg2} alt="person working" className="accent-img" />
+      </article>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.section`
