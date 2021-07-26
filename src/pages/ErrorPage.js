0da85f09e56d0e61cdb8e3972a40ff5d3880d1ea
munish710 <munish710@gmail.com>
2021-07-26 15:55:33 +0530

    added Error Page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -2,7 +2,17 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 const ErrorPage = () => {
-  return <h4>error page</h4>;
+  return (
+    <Wrapper className="page-100">
+      <section>
+        <h1>404</h1>
+        <h3>Sorry, the page you tried cannot be found</h3>
+        <Link to="/" className="btn">
+          back home
+        </Link>
+      </section>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.main`
@@ -12,7 +22,7 @@ const Wrapper = styled.main`
   align-items: center;
   text-align: center;
   h1 {
-    font-size: 10rem;
+    font-size: 9rem;
   }
   h3 {
     text-transform: none;
