599900956929bea61dc60cea10c3b3cac451624b
munish710 <munish710@gmail.com>
2021-07-28 23:10:47 +0530

    added ListView to Products page

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -2,8 +2,27 @@ import React from "react";
 import styled from "styled-components";
 import { formatPrice } from "../utils/helpers";
 import { Link } from "react-router-dom";
-const ListView = () => {
-  return <h4>list view</h4>;
+const ListView = ({ products }) => {
+  return (
+    <Wrapper>
+      {products.map((product) => {
+        const { id, image, name, price, description } = product;
+        return (
+          <article key={id}>
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <h5 className="price">{formatPrice(price)}</h5>
+              <p>{description.substring(0, 150)}...</p>
+              <Link to={`/products/${id}`} className="btn">
+                Details
+              </Link>
+            </div>
+          </article>
+        );
+      })}
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.section`
@@ -31,7 +50,7 @@ const Wrapper = styled.section`
     margin-bottom: 1rem;
   }
   .btn {
-    font-size: 0.5rem;
+    font-size: 0.75rem;
     padding: 0.25rem 0.5rem;
   }
   @media (min-width: 992px) {
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { FaHtml5 } from "react-icons/fa";
 import { useFilterContext } from "../context/filter_context";
 import GridView from "./GridView";
 import ListView from "./ListView";
 
 const ProductList = () => {
-  const { filteredProducts: products } = useFilterContext();
+  const { filteredProducts: products, gridView } = useFilterContext();
+  if (products.length < 1) {
+    return (
+      <h5 style={{ textTransform: "none" }}>
+        Sorry, no products matched your search :/
+      </h5>
+    );
+  }
+
+  if (gridView === false) {
+    return <ListView products={products} />;
+  }
   return <GridView products={products} />;
 };
 
