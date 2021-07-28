60e092c8bb969e0e72316b14d02056090e71e310
munish710 <munish710@gmail.com>
2021-07-29 00:44:40 +0530

    added the sort functionality to Products page

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -21,6 +21,28 @@ const filter_reducer = (state, action) => {
       return { ...state, gridView: true };
     case SET_LISTVIEW:
       return { ...state, gridView: false };
+    case UPDATE_SORT:
+      return { ...state, sort: action.payload };
+    case SORT_PRODUCTS:
+      const { sort, filteredProducts } = state;
+      let tempProducts = [...filteredProducts];
+      if (sort === "price-lowest") {
+        tempProducts = tempProducts.sort((a, b) => a.price - b.price);
+      }
+      if (sort === "price-highest") {
+        tempProducts = tempProducts.sort((a, b) => b.price - a.price);
+      }
+      if (sort === "name-a") {
+        tempProducts = tempProducts.sort((a, b) => {
+          return a.name.localeCompare(b.name);
+        });
+      }
+      if (sort === "name-z") {
+        tempProducts = tempProducts.sort((a, b) => {
+          return b.name.localeCompare(a.name);
+        });
+      }
+      return { ...state, filteredProducts: tempProducts };
   }
   throw new Error(`No Matching "${action.type}" - action type`);
 };
