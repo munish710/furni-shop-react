92c62c0a2fe22600ff0bf9eb0fe4f3eba16f08f1
munish710 <munish710@gmail.com>
2021-07-29 23:22:31 +0530

    added functionality to set unique values for filter

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -11,6 +11,11 @@ const Filters = () => {
     clearFilters,
     allProducts,
   } = useFilterContext();
+
+  const categories = getUniqueValues(allProducts, "category");
+  const companies = getUniqueValues(allProducts, "company");
+  const colors = getUniqueValues(allProducts, "colors");
+
   return (
     <Wrapper>
       <div className="content">
