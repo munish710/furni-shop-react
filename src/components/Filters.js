5272b8e623aa05b65d05f5a5b32e0b87277ddc16
munish710 <munish710@gmail.com>
2021-07-30 00:36:16 +0530

    added filter by colors

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -85,6 +85,46 @@ const Filters = () => {
             </select>
           </div>
           {/* Companies end*/}
+
+          {/* Colors */}
+          <div className="form-control">
+            <h5>colors</h5>
+            <div className="colors">
+              {colors.map((c, index) => {
+                if (c === "all") {
+                  return (
+                    <button
+                      key={index}
+                      name="color"
+                      onClick={updateFilters}
+                      data-color="all"
+                      className={`${
+                        color === "all" ? "all-btn active" : "all-btn"
+                      }`}
+                    >
+                      all
+                    </button>
+                  );
+                }
+                return (
+                  <button
+                    key={index}
+                    name="color"
+                    style={{ background: c }}
+                    className={`${
+                      color === c ? "color-btn acctive" : "color-btn"
+                    }`}
+                    data-color={c}
+                    onClick={updateFilters}
+                  >
+                    {color === c ? <FaCheck /> : null}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+
+          {/* Colors end */}
         </form>
       </div>
     </Wrapper>
