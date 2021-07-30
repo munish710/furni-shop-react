84c6f514c6e054278e5d8cd0235d419a47314402
munish710 <munish710@gmail.com>
2021-07-30 16:40:00 +0530

    added localstorage persistence for cart items

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -8,8 +8,17 @@ import {
   COUNT_CART_TOTALS,
 } from "../actions";
 
+const getLocalStorage = () => {
+  let cart = localStorage.getItem("cart");
+  if (cart) {
+    return JSON.parse(localStorage.getItem("cart"));
+  } else {
+    return [];
+  }
+};
+
 const initialState = {
-  cart: [],
+  cart: getLocalStorage(),
   totalItems: 0,
   totalAmount: 0,
   shippingFee: 534,
@@ -29,6 +38,10 @@ export const CartProvider = ({ children }) => {
   const toggleAmount = (id, value) => {};
 
   const clearCart = () => {};
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state.cart));
+  }, [state, cart]);
   return (
     <CartContext.Provider
       value={{ ...state, addToCart, removeItem, toggleAmount, clearCart }}
