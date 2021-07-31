6cc0d2e131fca155faf6cdc131145642323dff9a
munish710 <munish710@gmail.com>
2021-07-31 17:36:59 +0530

    added  checkout page protection

--- src/components/CartTotals.js
+++ src/components/CartTotals.js
@@ -7,6 +7,8 @@ import { Link } from "react-router-dom";
 
 const CartTotals = () => {
   const { totalAmount, shippingFee } = useCartContext();
+  const { myUser, loginWithRedirect } = useUserContext();
+
   return (
     <Wrapper>
       <div>
@@ -22,9 +24,15 @@ const CartTotals = () => {
             order total :<span> {formatPrice(totalAmount + shippingFee)} </span>
           </h4>
         </article>
-        <Link to="/checkout" className="btn">
-          proceed to checkout
-        </Link>
+        {myUser ? (
+          <Link to="/checkout" className="btn">
+            proceed to checkout
+          </Link>
+        ) : (
+          <button type="button" onClick={loginWithRedirect} className="btn">
+            Login
+          </button>
+        )}
       </div>
     </Wrapper>
   );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -10,6 +10,7 @@ import { useUserContext } from "../context/user_context";
 
 const Sidebar = () => {
   const { isSidebarOpen, closeSidebar } = useProductsContext();
+  const { myUser } = useUserContext();
   return (
     <SidebarContainer>
       <aside
@@ -31,9 +32,11 @@ const Sidebar = () => {
               </li>
             );
           })}
-          <li>
-            <Link to="/checkout">Checkout</Link>
-          </li>
+          {myUser && (
+            <li>
+              <Link to="/checkout">checkout</Link>
+            </li>
+          )}
         </ul>
         <CartButtons />
       </aside>
