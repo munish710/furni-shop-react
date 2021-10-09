6b9c7576a4a0783872b671641d255b89326572e8
munish710 <munish710@gmail.com>
2021-10-09 15:04:15 +0530

    refactor : Footer Component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,38 +1,78 @@
 import React from "react";
+import {
+  AiOutlineGithub,
+  AiOutlineTwitter,
+  AiOutlineLinkedin,
+} from "react-icons/ai";
 import styled from "styled-components";
+
 const Footer = () => {
   return (
     <Wrapper>
-      <h5>
-        &copy; {new Date().getFullYear()}
-        <span> ComfySloth. </span>
-      </h5>
-      <h5>All Rights Reserved.</h5>
+      <div className="section-center footer-center">
+        <h5>
+          Made with <span className="code">&lt;/&gt;</span> by Munish Thakur
+        </h5>
+        <div className="footer-icons">
+          <a
+            href="https://github.com/munish710"
+            target="_blank"
+            className="footer-icon"
+            rel="noopener noreferrer"
+          >
+            <AiOutlineGithub />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/munish-thakur-a14078190/"
+            target="_blank"
+            className="footer-icon"
+            rel="noopener noreferrer"
+          >
+            <AiOutlineLinkedin />
+          </a>
+          <a
+            href="https://twitter.com/munish710"
+            target="_blank"
+            className="footer-icon"
+            rel="noopener noreferrer"
+          >
+            <AiOutlineTwitter />
+          </a>
+        </div>
+      </div>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.footer`
-  height: 5rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
   background: var(--clr-black);
-  text-align: center;
-  span {
-    color: var(--clr-primary-5);
+  margin-top: 2rem;
+  .code {
+    color: var(--clr-red-dark);
   }
-  h5 {
-    color: var(--clr-white);
-    margin: 0.1rem;
 
-    font-weight: 400;
-    text-transform: none;
-    line-height: 1.25;
+  .footer-center {
+    padding: 1.5rem 0.75rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    h5 {
+      color: var(--clr-white);
+      margin-bottom: 1rem;
+      font-weight: 400;
+      font-size: 1rem;
+    }
   }
-  @media (min-width: 776px) {
-    flex-direction: row;
+
+  .footer-icon {
+    color: var(--clr-primary-4);
+    font-size: 1.5rem;
+    margin-right: 2rem;
+  }
+
+  .footer-icon:hover {
+    color: var(--clr-primary-5);
   }
 `;
 
